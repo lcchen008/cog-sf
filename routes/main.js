61d4495f979606b6aeb5ce89af5378d761e39032
cgo2016 <chen.1996@osu.edu>
2016-12-04 12:00:07 -0800

    .

--- routes/main.js
+++ routes/main.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('main', { title: 'California International' });
+  res.render('main', { title: 'CBA Education' });
 });
 
 module.exports = router;
